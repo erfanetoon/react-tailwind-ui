4fe403f4dcff4317952250e4b973039411641ff8
Erfan Hosseinpoor <erfanhoseinpor@gmail.com>
2024-12-07 14:00:19 +0330

    :sparkles: update withTailwind function file

---
+++ src/withTailwind.ts
@@ -0,0 +1,26 @@
+import { Config } from "tailwindcss";
+import merge from "deepmerge";
+import shadows from "./config/shadows";
+import colors from "./config/colors";
+import fontSizes from "./config/fontSizes";
+import breakpoints from "./config/breakpoints";
+
+const withTailwind = (tailwindConfig: Config) => {
+    return merge(
+        {
+            darkMode: "class",
+            content: [
+                "./node_modules/@erfanetoon/react-tailwind-ui/**/*.{js,ts,jsx,tsx}",
+            ],
+            theme: {
+                boxShadow: shadows,
+                colors: colors,
+                fontSize: fontSizes,
+                screens: breakpoints,
+            },
+        },
+        { ...tailwindConfig },
+    );
+};
+
+export default withTailwind;
