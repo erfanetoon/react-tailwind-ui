b6b31e8e8e1eb2f472297a914848a96ef125eba5
Erfan Hosseinpoor <erfanhoseinpor@gmail.com>
2024-12-30 17:13:13 +0330

    :sparkles: add useResponsive hook and export all config files from index file

--- src/index.ts
+++ src/index.ts
@@ -11,33 +11,33 @@ export type { IThemeContext, TThemeAction } from "./context/types";
 // Our tailwind base config
 export { default as withTailwind } from "./withTailwind";
 
+// Configs
+export * from "./config/breakpoints";
+export * from "./config/colors";
+export * from "./config/fontSizes";
+export * from "./config/shadows";
+
 // Components
 export * from "./components/button";
 export type { IButtonProps } from "./components/button/types";
-
 export * from "./components/chip";
 export type { IChipProps } from "./components/chip/types";
-
 export * from "./components/codeInput";
 export type { ICodeInputProps } from "./components/codeInput/types";
-
 export * from "./components/dropdown";
 export type { IDropdownProps } from "./components/dropdown/types";
-
 export * from "./components/iconButton";
 export type { IIconButtonProps } from "./components/iconButton/types";
-
 export * from "./components/input";
 export type { IInputProps } from "./components/input/types";
-
 export * from "./components/hint";
 export type { IHintProps } from "./components/hint/types";
-
 export * from "./components/passwordInput";
 export type { IPasswordInputProps } from "./components/passwordInput/types";
-
 export * from "./components/textarea";
 export type { ITextareaProps } from "./components/textarea/types";
-
 export * from "./components/tooltip";
 export type { ITooltipProps } from "./components/tooltip/types";
+
+// Hooks
+export * from "./hooks/useResponsive";
